e0167515dc238cf37b0350016b764c113096f436
Shuhhh <63115888+Shuhhh@users.noreply.github.com>
2021-10-05 17:23:53 +0300

    5.10 frontend

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <router-view />
-    
+<!--    <RegisterPage></RegisterPage>-->
     </div>
 </template>
 
 <script>
 
+
 export default {
   name: 'app',
   components: {
-   
   },
 }
 
