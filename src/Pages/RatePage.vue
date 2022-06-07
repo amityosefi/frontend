0364f5c7b2500cac3fb97742f6c48f76e53955ff
Daniel <zamanski@post.bgu.ac.il>
2022-06-07 19:30:23 +0300

    fixed rate and submit issues

--- src/Pages/RatePage.vue
+++ src/Pages/RatePage.vue
@@ -181,6 +181,19 @@ export default {
           try {
             console.log("submitted 6");
             await this.submit_regular(6);
+            if(this.$root.store.extra_pics.length == 0)
+            {
+               localStorage.setItem("is_done", true);
+              this.$root.store.is_done = true;
+          
+              await this.axios.post(
+              this.$root.store.address + `images/is_done`,
+              {
+                is_done: true,
+                user_id: this.$root.store.u_id
+              }
+              );
+            }
           } catch (err) {
             console.log(err.response);
           }
@@ -227,8 +240,9 @@ export default {
         data_ratings: rates,
         id: user_id,
       });
-
-      if (this.$root.store.is_done == true || this.$root.store.extra_pics.length == 0) {
+      console.log("is done?",this.$root.store.is_done)
+      console.log("is really done?",Boolean(this.$root.store.is_done) == true )
+      if (Boolean(this.$root.store.is_done) == true ) {
         this.showModal("mymodalC"); 
       }
       else {
@@ -285,19 +299,7 @@ export default {
         for (var j = 6; j < len; j++) {
           temp.push(this.$root.store.extra_pics[j]);
         }
-        if (temp.length == 0) {
-          localStorage.setItem("is_done", true);
-          this.$root.store.is_done = true;
-          
-          await this.axios.post(
-          this.$root.store.address + `images/is_done`,
-            {
-              is_done: true,
-              user_id: this.$root.store.u_id
-            }
-          );
-          
-        }
+        
         this.$root.store.extra_pics = temp;
         localStorage.extra_pics = JSON.stringify(this.$root.store.extra_pics);
 
@@ -319,7 +321,7 @@ export default {
         this.Images = arr;
         this.Bins = arr2;
         this.isLoading = false;
-        this.showModal();
+        // this.showModal();
       } catch (err) {
         console.log(err.response);
       }
@@ -399,7 +401,7 @@ export default {
       }
       let ranked_a  = localStorage.RankedImages == undefined;
       let submitted_a = Boolean(this.$root.store.is_submitted) == true;
-      console.log("is done?",this.$root.store.is_submitted);
+      console.log("is submitted?",this.$root.store.is_submitted);
       console.log("why?", submitted_a);
       // let ranked_b = typeof(localStorage.RankedImages) == "undefined";
       // let submitted_b = typeof(localStorage.is_submitted) == "undefined";
